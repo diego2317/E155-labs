7d5993070bc8a0b383a62b72d151ec06be19d7fe
Diego Weiss <diegokweiss@gmail.com>
2025-08-30 00:25:33 -0700

    Design synthesizes now

--- lab1/fpga/src/lab1_dw.sv
+++ lab1/fpga/src/lab1_dw.sv
@@ -5,10 +5,11 @@
 // E155 development board and a module that controls a 7-segment display
 
 module lab1_dw(input  logic clk,
+			   input  logic reset,
 			   input  logic [3:0] s,
 			   output logic [2:0] led,
 			   output logic [6:0] seg);
 			   
-			   led_controller LED (clk, s, led[2:0);
-			   display_controller DISPLAY (clk, s, seg[6:0];
+			   led_controller LED (clk, reset, s, led);
+			   display_controller DISPLAY (s, seg);
 endmodule
