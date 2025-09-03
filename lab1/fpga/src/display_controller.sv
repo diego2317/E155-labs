72bb4f47a012caa6b2b7a39c686329db4850ca6e
Diego Weiss <98351752+diego2317@users.noreply.github.com>
2025-09-03 12:18:40 -0700

    fixed light up logic for 6

--- lab1/fpga/src/display_controller.sv
+++ lab1/fpga/src/display_controller.sv
@@ -15,7 +15,7 @@ module display_controller(
 			4'h3: seg = 7'b0110000;
 			4'h4: seg = 7'b0011001;
 			4'h5: seg = 7'b0010010;
-			4'h6: seg = 7'b0100010;
+			4'h6: seg = 7'b0000010;
 			4'h7: seg = 7'b1111000;
 			4'h8: seg = 7'b0000000;
 			4'h9: seg = 7'b0011000;
