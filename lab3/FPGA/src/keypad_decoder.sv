766918ae4e9bd7d01df15702d93522901e23b9a4
Diego Weiss <diegokweiss@gmail.com>
2025-09-16 22:13:01 -0700

    Changed logic back

--- lab3/FPGA/src/keypad_decoder.sv
+++ lab3/FPGA/src/keypad_decoder.sv
@@ -11,22 +11,22 @@ module keypad_decoder(
 
 	always_comb begin
 		case ({cols, rows})
-            8'b1110_0001: input_key = 4'h0;
-            8'b1101_0001: input_key = 4'h1;
-            8'b1011_0001: input_key = 4'h2;
-            8'b0111_0001: input_key = 4'h3;
-            8'b1110_0010: input_key = 4'h4;
-            8'b1101_0010: input_key = 4'h5;
-            8'b1011_0010: input_key = 4'h6;
-            8'b0111_0010: input_key = 4'h7;
-            8'b1110_0100: input_key = 4'h8;
-            8'b1101_0100: input_key = 4'h9;
-            8'b1011_0100: input_key = 4'hA;
-            8'b0111_0100: input_key = 4'hB;
-            8'b1110_1000: input_key = 4'hC;
-            8'b1101_1000: input_key = 4'hD;
-            8'b1011_1000: input_key = 4'hE;
-            8'b0111_1000: input_key = 4'hF;
+            8'b1110_1110: input_key = 4'h0;
+            8'b1101_1110: input_key = 4'h1;
+            8'b1011_1110: input_key = 4'h2;
+            8'b0111_1110: input_key = 4'h3;
+            8'b1110_1101: input_key = 4'h4;
+            8'b1101_1101: input_key = 4'h5;
+            8'b1011_1101: input_key = 4'h6;
+            8'b0111_1101: input_key = 4'h7;
+            8'b1110_1011: input_key = 4'h8;
+            8'b1101_1011: input_key = 4'h9;
+            8'b1011_1011: input_key = 4'hA;
+            8'b0111_1011: input_key = 4'hB;
+            8'b1110_0111: input_key = 4'hC;
+            8'b1101_0111: input_key = 4'hD;
+            8'b1011_0111: input_key = 4'hE;
+            8'b0111_0111: input_key = 4'hF;
             default: input_key = 4'bx;
 		endcase
 	end
