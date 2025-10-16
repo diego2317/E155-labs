a2b5433ed31227f75ab3ceccf377c9bd017170c7
Diego Weiss <diegokweiss@gmail.com>
2025-10-15 18:54:42 -0700

    setup

---
+++ lab6/mcu/lib/DS1722.h
@@ -0,0 +1,5 @@
+// DS1722.h
+// Diego Weiss
+// dweiss@hmc.edu
+// 10/15/25
+// TODO: <SHORT DESCRIPTION OF WHAT THIS FILE DOES>
---
+++ lab6/mcu/lib/STM32L432KC_SPI.c
@@ -0,0 +1,5 @@
+// STM32L432KC_SPI.c
+// Diego Weiss
+// dweiss@hmc.edu
+// 10/15/25
+// Source code for SPI functions
